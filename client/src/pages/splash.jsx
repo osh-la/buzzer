13288f418ff05fdbc48005ef3d76d30dd506c26f
abimiku oshela <abimikuoshela@gmail.com>
2026-04-10 13:22:26 +0100

    splash

--- client/src/pages/splash.jsx
+++ client/src/pages/splash.jsx
@@ -15,7 +15,7 @@ export default function Splash() {
 
   return (
     <div className="h-screen flex items-center justify-center text-7xl font-bold bg-black text-white">
-      ERICa!!!
+      ERIC!!!
     </div>
   );
 }
