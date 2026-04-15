5cd5ad5261638f6c8a3cfa936a0b0454adbe3ee6
abimiku oshela <abimikuoshela@gmail.com>
2026-04-15 09:56:56 +0100

    plenty things

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full text-center py-4 text-sm text-white border-t mt-10">
+    <footer className="w-full text-center py-4 text-sm text-gray-100 border-t mt-10">
      © osh_la™ 
     </footer>
   );
