8407023b2bfda84172791488d75440676c5f28ac
abimiku oshela <abimikuoshela@gmail.com>
2026-04-15 07:30:42 +0100

    footer

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Splash from './pages/splash';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Main Routes */}
-        <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+      <div className="min-h-screen flex flex-col">
+        
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Splash />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
 
-        {/* Catch all unknown routes */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
