c91f95e925ecd31c245bd043ce433094f4d27871
abimiku oshela <abimikuoshela@gmail.com>
2026-04-15 10:17:47 +0100

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 export default function Footer() {
   return (
-    <footer className="w-full bg-red-400 text-center py-4 text-sm text-gray-100 border-t mt-10">
-     © osh_la™ 
+    <footer className="w-full text-centertext-sm text-gray-100 ">
+     © osh_la
     </footer>
   );
 }
