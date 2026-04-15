03d6aa7d9b3e4dc0cdeb0ecd10b6b757382eaab4
abimiku oshela <abimikuoshela@gmail.com>
2026-04-15 07:41:48 +0100

    footer wahala

--- client/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full text-center py-4 text-sm text-gray-500 border-t mt-10">
-     © osh_la™ 
-    </footer>
-  );
-}
---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-center py-4 text-sm text-white border-t mt-10">
+     © osh_la™ 
+    </footer>
+  );
+}
